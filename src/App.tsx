7898dbb3db585344c4e575957efbaa451fa08282
NickGreenSF <nicholasgreensf@gmail.com>
2022-05-26 00:56:42 -0700

    hm maybe i'll make a table

--- src/App.tsx
+++ src/App.tsx
@@ -17,9 +17,38 @@ const TitleBar = styled.div`
   left: 0;
   margin-top: 0;
   position: sticky;
-  border: 1px solid black;
-  border-bottom: 0px;
   background-color: white;
+  text-align: center;
+  z-index: 3;
+`
+
+const TitleText = styled.div`
+  padding-top: ${height / 30}px;
+`
+
+const DateBar = styled.div`
+  width: ${width}px;
+  height: ${height / 40}px;
+  font-size: ${height / 50}px;
+  text-align: center;
+  position: relative;
+`
+
+const FruitLabel = styled.div`
+  margin-left: ${width / 10}px;
+`
+
+const FruitBar = styled.div`
+  width: ${width * .8}px;
+  height: ${height / 4}px;
+  background-color: white;
+  margin-left: ${width / 10}px;
+  margin-right: ${width / 10}px;
+  font-size: ${width / 40}px;
+  text-align: center;
+  position: relative;
+  display: grid;
+  grid-template-columns: 1fr 2fr;
 `
 
 const TopLevel = styled.div`
@@ -29,25 +58,57 @@ const TopLevel = styled.div`
   top: 0;
 `
 
+const PictureOfFood = styled.img`
+  max-height: ${height / 4 - 10}px;
+  max-width: ${width / 9}px;
+  margin-top: 5px;
+  z-index: 2;
+  :hover{
+    z-index: 3;
+    transform: scale(1.1, 1.1);
+  }
+`
+
+const TopOfTable = styled.div`
+  width: ${width / 8}px;
+  height: ${height / 20}px;
+  margin-left: ${width / 10}px;
+  margin-top: ${height / 80}px;
+  background-color: brown;
+`
+
+const BottomOfTable = styled.div`
+  width: ${width / 8}px;
+  height: ${height / 20}px;
+  margin-left: ${width / 10}px;
+  margin-bottom: ${height / 80}px;
+  background-color: brown;
+`
+
 function App() {
   return (
     <TopLevel>
-      <TitleBar>text</TitleBar>
+      <TitleBar>
+        <TitleText>
+          WHAT I ATE
+        </TitleText>
+        </TitleBar>
       {Object.keys(Food).map((function(key, index){
         let day: string[][] = Food[key];
-        return day.map(function(meal, index){
-          console.log("element " + meal + " index " + index)
-          if (index % 2 == 0){
+        return <div>
+          <TopOfTable />
+          {day.map(function(meal, index){
+            console.log("element " + meal + " index " + index)
             return <div>
-              {meal[0] + meal[1]}
-              <img src={meal[2]} alt="here's a THING of it"></img>
+              <FruitLabel>{meal[0]}</FruitLabel>
+              <FruitBar>
+                <PictureOfFood src={meal[2]} alt="here's a THING of it"></PictureOfFood>
+                {meal[1]}
+              </FruitBar>
             </div>
-          }
-          return <div>
-            <img src={meal[2]} alt="here's a THING of it"></img>
-            {meal[0] + meal[1]}
-          </div>
-        })
+          })}
+          <BottomOfTable />
+        </div>
       }))}
     </TopLevel>
   );
--- src/constants/Food.tsx
+++ src/constants/Food.tsx
@@ -4,5 +4,7 @@ interface Map {
   }
 
 export const Food: Map = {
-    "May 24" : [["11 AM", "2 raspberry Pop-Tarts", "good!"], ["2 PM", "Spaghetti", "bad!"]]
+    "May 25" : [["1 PM", "3 pieces of leftover Little Caesars", "https://www.gannett-cdn.com/presto/2022/01/02/PDTF/223b571e-5713-48f9-b11d-50268ee85801-PEP_UPGRD_PULL_2.jpg"]],
+    "May 24" : [["11 AM", "2 raspberry Pop-Tarts", "https://media.istockphoto.com/photos/toaster-pastries-picture-id1181684179?k=20&m=1181684179&s=612x612&w=0&h=v8JV1MP_RZdmbly2g9bYAOfR3GKEFjjddko3XSb7OCw="],
+     ["2 PM", "Spaghetti", "https://media.istockphoto.com/photos/spaghetti-bolognese-picture-id636746848?k=20&m=636746848&s=612x612&w=0&h=bz2Jruoc5y265CGnUVwjnBFMcWb1ruGXCaM0eAqyC2Q="]]
 }
