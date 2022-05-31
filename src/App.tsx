db1916204806037c016912377fb9803c24aa70a7
NickGreenSF <nicholasgreensf@gmail.com>
2022-05-30 23:03:31 -0700

    basing some changes on the first result i found when i googled food blog

--- src/App.tsx
+++ src/App.tsx
@@ -26,68 +26,48 @@ const TitleText = styled.div`
   padding-top: ${height / 30}px;
 `
 
-const DateBar = styled.div`
-  width: ${width}px;
-  height: ${height / 40}px;
-  font-size: ${height / 50}px;
-  text-align: center;
+const CerealBox = styled.div`
   position: relative;
-`
-
-const FruitLabel = styled.div`
-  margin-left: ${width / 10}px;
-`
-
-const FruitBar = styled.div`
-  width: ${width * .8}px;
-  height: ${height / 4}px;
-  background-color: white;
-  margin-left: ${width / 10}px;
-  margin-right: ${width / 10}px;
-  font-size: ${width / 40}px;
-  text-align: center;
-  position: relative;
-  display: grid;
-  grid-template-columns: 1fr 2fr;
-`
-
-const TopLevel = styled.div`
-  width: 100%;
-  position: absolute;
-  left: 0;
-  top: 0;
-`
-
-const PictureOfFood = styled.img`
-  max-height: ${height / 4 - 10}px;
-  max-width: ${width / 9}px;
-  margin-top: 5px;
-  z-index: 2;
-  :hover{
-    z-index: 3;
-    transform: scale(1.1, 1.1);
+  width: ${width * .4}px;
+  margin-left: ${width * .05}px;
+  height: ${height * .4}px;
+  overflow: hidden;
+  @media screen and (max-width: 1200px){
+    width: ${width * .9}px;
   }
 `
 
-const TopOfTable = styled.div`
-  width: ${width / 8}px;
-  height: ${height / 20}px;
-  margin-left: ${width / 10}px;
-  margin-top: ${height / 80}px;
-  background-color: brown;
+const PictureOfFood = styled.img`
+  position: absolute;
+  min-width: 100%;
+  min-height: 100%;
+  z-index: -1;
 `
 
-const BottomOfTable = styled.div`
-  width: ${width / 8}px;
-  height: ${height / 20}px;
-  margin-left: ${width / 10}px;
-  margin-bottom: ${height / 80}px;
-  background-color: brown;
+const TextBacker = styled.div`
+  background-color: black;
+  color: white;
+  margin-top: ${height * .3}px;
+  width: 33%;
+  margin-left: 10%;
+  font-size: 20px;
+`
+
+const Text = styled.div`
+  margin: 5px;
+`
+
+const Day = styled.div`
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  @media screen and (max-width: 1200px){
+    grid-template-columns: 1fr;
+  }
 `
 
 function App() {
   return (
-    <TopLevel>
+    <div>
       <TitleBar>
         <TitleText>
           WHAT I ATE
@@ -95,22 +75,20 @@ function App() {
         </TitleBar>
       {Object.keys(Food).map((function(key, index){
         let day: string[][] = Food[key];
-        return <div>
-          <TopOfTable />
+        return <div>{key}<Day>
           {day.map(function(meal, index){
             console.log("element " + meal + " index " + index)
-            return <div>
-              <FruitLabel>{meal[0]}</FruitLabel>
-              <FruitBar>
-                <PictureOfFood src={meal[2]} alt="here's a THING of it"></PictureOfFood>
-                {meal[1]}
-              </FruitBar>
-            </div>
+            return <CerealBox>
+              <PictureOfFood src={meal[2]} alt="here's a THING of it"></PictureOfFood>
+              <TextBacker>
+                <Text>{meal[1]}</Text>
+              </TextBacker>
+            </CerealBox>
           })}
-          <BottomOfTable />
+        </Day>
         </div>
       }))}
-    </TopLevel>
+    </div>
   );
 }
 
